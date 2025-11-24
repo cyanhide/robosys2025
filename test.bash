66e737f9534b7e5c532fcd06f44a0ebd3ef39d28
cyanhide <core.editor>
2025-11-24 18:11:54 +0900

    Test command

---
+++ test.bash
@@ -0,0 +1,26 @@
+#!/bin/bash -xv
+# SPDX-FileCopyrightText: 2025 Hidenori Koseki
+# SPDX-License-Identifier: BSD-3-Clause
+
+ng () {
+	echo ${1}行目が違うよ
+	res=1
+}
+
+res=0
+
+### NORMAL INPUT ###
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng "$LINENO"
+
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ] 	  || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+[ "${res}" = 0 ] && echo OK
+exit $res
